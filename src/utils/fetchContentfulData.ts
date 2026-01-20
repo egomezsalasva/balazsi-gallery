7e56a327949d7d4ec702c3fd83734854df419453
Kike <kike@Kikes-MacBook-Pro.local>
2026-01-20 14:12:47 +0100

    change env vars

--- src/utils/fetchContentfulData.ts
+++ src/utils/fetchContentfulData.ts
@@ -3,12 +3,12 @@ export const fetchContentfulData = async (
   variables?: Record<string, unknown>,
 ) => {
   const res = await fetch(
-    `https://graphql.contentful.com/content/v1/spaces/${process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID}`,
+    `https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}`,
     {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        Authorization: `Bearer ${process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN}`,
+        Authorization: `Bearer ${process.env.CONTENTFUL_ACCESS_TOKEN}`,
       },
       body: JSON.stringify({ query, variables }),
     },
